83940aae60c181ff84d0ad3c052c950b405e86f5
KaisGitHub02 <119108982+KaisGitHub02@users.noreply.github.com>
2024-06-30 11:48:04 +0200

    Update AddAsset.js

--- front-dapp/src/AddAsset.js
+++ front-dapp/src/AddAsset.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function AddAsset() {
+function AddAsset({ token }) {
   const [id, setId] = useState('');
   const [tradeDate, setTradeDate] = useState('');
   const [buyer, setBuyer] = useState('');
